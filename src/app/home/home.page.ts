d80ad497b6b580b5deb4684f1526cb7961a9a504
AlyDap <ferdynan.jr@gmail.com>
2024-01-14 10:06:08 +0700

    aaaa

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
   constructor() {}
+
+  public data = [
+    'Amsterdam',
+    'Buenos Aires',
+    'Cairo',
+    'Geneva',
+    'Hong Kong',
+    'Istanbul',
+    'London',
+    'Madrid',
+    'New York',
+    'Panama City',
+  ];
+  public results = [...this.data];
+
+  handleInput(event: any) {
+    const query = event.detail.value.toLowerCase();
+    this.results = this.data.filter((d) => d.toLowerCase().indexOf(query) > -1);
+  }
 }
